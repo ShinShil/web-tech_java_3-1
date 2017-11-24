a56b565cb1f6ddad89cc5e0a31c5a1f28b02aaa3
Nikita <Starostin.450503@gmail.com>
2017-11-24 12:32:58 +0300

    add email sender

--- vt_lr1/src/business/AuthProvider.java
+++ vt_lr1/src/business/AuthProvider.java
@@ -7,10 +7,12 @@ public class AuthProvider {
     public static AuthManager authManager;
     public static PasswordAuth passwordAuth;
     public static PermissionManager permissionManager;
+    public static EmailSender emailSender;
 
     static {
         authManager = new AuthManager();
         passwordAuth = new PasswordAuth();
         permissionManager = new PermissionManager();
+        emailSender = new EmailSender();
     }
 }
